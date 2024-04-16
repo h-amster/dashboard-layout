b90af88a7594db9eda16d39d7235ca7d10bdcaec
Andrii Khomiak <an.khomiak@gmail.com>
2024-04-16 10:47:06 +0300

    make build

---
+++ index.c9d424dd.js
@@ -0,0 +1,2 @@
+var e,t;e=document.querySelector(".main__btn"),t=document.querySelector(".header"),e.addEventListener("click",function(){t.classList.toggle("header--visible"),e.classList.toggle("main__btn--open"),document.body.classList.toggle("page__body--locked")});
+//# sourceMappingURL=index.c9d424dd.js.map
---
+++ index.f61938ee.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".main__btn"),t=document.querySelector(".header");e.addEventListener("click",()=>{t.classList.toggle("header--visible"),e.classList.toggle("main__btn--open"),document.body.classList.toggle("page__body--locked")});
+//# sourceMappingURL=index.f61938ee.js.map
